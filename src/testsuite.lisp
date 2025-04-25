fbc547886c5e9f4635bd510e209fa18ac5380de1
Viktor T. Toth <vttoth@vttoth.com>
2025-04-25 02:58:05 -0400

    Missing return types added to fix GCL 2.7.1 compatibility.
    As suggested by Camm Maguire.
    
    Also removed a test that no longer fails under GCL from the expected failures list.

--- src/testsuite.lisp
+++ src/testsuite.lisp
@@ -279,7 +279,7 @@
     ((mlist simp) "rtest_abs_integrate" ((mlist) 173 249))
     "rtest_pochhammer"
     ((mlist simp) "rtest_to_poly_solve"
-     #+gcl ((mlist simp) 64 74 80 116 140 141 168 184 212 242 245 322)
+     #+gcl ((mlist simp) 64 74 80 116 140 141 168 184 242 245 322)
      #-(or gcl) ((mlist simp) 64 74 80 116 140 141 168 184 242 245 322)
      )
     ((mlist simp) "rtest_sym"
