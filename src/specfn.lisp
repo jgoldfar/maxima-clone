ad679f91c9526dd393b00b4c78f48c40cbf012dc
Raymond Toy <toy.raymond@gmail.com>
2025-08-02 09:36:46 -0700

    Tell Maxima about more properties of kbateman
    Maxima now knows about some of the special properties of the kbateman
    function, including some special values and expressions in terms of
    elementary functions.
    
    See the references in the comments.

--- src/specfn.lisp
+++ src/specfn.lisp
@@ -933,6 +933,51 @@
       (t
        (give-up)))))
 
+;; References:
+;;  - https://en.wikipedia.org/wiki/Bateman_function 
+;;  - https://www.researchgate.net/publication/350992070_The_Bateman_Functions_Revisited_After_90_Years_--_A_Survey_of_Old_and_New_Results
+;;
+;; 
+(def-simplifier (kbateman :subfun-arglist (v)) (z)
+  (let ((vn (integer-representation-p v)))
+    (cond
+      ((and vn (>= vn 0))
+       ;; kbateman[n](z), for integer n >= 0
+       (let ((signz (asksign z)))
+         (cond
+           ((eq signz '$zero)
+            ;; kbateman[n](0) = 2/%pi/n*sin(%pi*n/2)
+            (if (zerop vn)
+                0
+                (mul (div 2 (mul '$%pi vn))
+                     (ftake '%sin (div (mul '$%pi vn) 2)))))
+           ((and (evenp vn) (eq signz '$positive))
+            ;; kbateman[2*n](x) = (-1)^n*x*exp(x)/n!*diff(x^(n-1)*exp(-2*x),x,n)
+            ;; for x >= 0.
+            (let ((n (/ vn 2))
+                  (zarg (gensym)))
+              ;; Handle small n directly and use the formula for larger values.
+              (cond
+                ((= n 0)
+                 (ftake '%exp (neg z)))
+                ((= n 1)
+                 (mul 2 z (ftake '%exp (neg z))))
+                ((= n 2)
+                 (mul 2 z (sub z 1)
+                      (ftake '%exp (neg z))))
+                (t
+                 ($factor
+                  ($substitute z zarg
+                               (mul
+                                (div (mul (expt -1 n) zarg (ftake '%exp zarg))
+                                     (ftake '%gamma (add 1 n)))
+                                ($diff (mul (power zarg (sub n 1))
+                                            (ftake '%exp (mul -2 zarg)))
+                                       zarg
+                                       n))))))))
+           (t (give-up)))))
+      (t (give-up)))))
+
 (in-package "BIGFLOAT")
 
 (defvar *debug-li-eval* nil)
@@ -1290,6 +1335,7 @@
 	   (values (polylog-log-series s z)))
 	  ((> (abs z) 1.5)
 	   (polylog-inversion-formula s z)))))
+
 
 ;;; Computation of Catalan's constant
 (in-package #:bigfloat)
@@ -1327,3 +1373,4 @@
               (+ sum a)))
         ((< (abs a) limit)
          (maxima::to (/ sum 2))))))
+
