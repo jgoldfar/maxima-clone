14e87d819bc741dcb4e1e2196a2394ee5960cb13
Leo Butler <l_butler@users.sourceforge.net>
2026-03-17 09:26:58 -0500

    mathml.lisp: Fix bug #4683
    lurkmathml does not typeset z[2]^3 correctly
    
    We copy a test from TEX-MEXPT that ensures the correct typesetting.

--- share/contrib/lurkmathml/mathml.lisp
+++ share/contrib/lurkmathml/mathml.lisp
@@ -418,6 +418,7 @@ Ref: https://developer.mozilla.org/en-US/docs/Web/MathML/Reference/Values#Consta
 			  (member (get-first-char f) '(#\% #\$) :test #'char=) ;; insist it is a % or $ function
                           (not (member f '(%sum %product %derivative %integrate %at
                                          %lsum %limit) :test #'eq)) ;; what else? what a hack...
+			  (not (member 'array (cdar fx) :test #'eq)) ; fix for x[i]^2
 			  (or (and (atom expon) (not (numberp expon))) ; f(x)^y is ok
 			      (and (atom expon) (numberp expon) (> expon 0))))))
 			      ; f(x)^3 is ok, but not f(x)^-1, which could
